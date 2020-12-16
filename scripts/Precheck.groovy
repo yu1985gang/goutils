daa7a27968f14b834f7e5122ecad80d2711cc8c4
ganyu <gerald.yu@nokia-sbell.com>
2020-12-16 15:41:35 +0800

    add files

--- scripts/Precheck.groovy
+++ scripts/Precheck.groovy
@@ -105,7 +105,7 @@ def pingAddress(String address, int account = 3) {
     def rc = null
     if(isIPv4(address)){
         rc = sh script: "ping -c ${account} ${address}", returnStatus: true, label: "Ping address"
-    }else (isIPv6(address)){
+    }else if (isIPv6(address)){
         def Interface = Utils.shCmd('/sbin/route -n | grep "^0.0.0.0" | rev | cut -d \' \' -f1 | rev', "Get IPV6 interface")
         rc = sh script: "ping6 -${Interfacc}  xx  -c ${account} ${address}", returnStatus: true, label: "Ping ipv6 address"
     }else{
