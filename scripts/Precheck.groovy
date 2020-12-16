562b6f648032107738a77f737d9971b31d6d6d1b
ganyu <gerald.yu@nokia-sbell.com>
2020-12-16 23:11:04 +0800

    add files

--- scripts/Precheck.groovy
+++ scripts/Precheck.groovy
@@ -139,8 +139,9 @@ def pingNE(String address){
         echo "routeInterface is $routeInterface"
         echo "address class is ${address.getClass()}"
         assert address instanceof String
+        echo "address is ${address}"
         echo "address is $address"
-        rc = sh script: "ssh -i /var/node.pem ${sshUserName}@${ncmHost} ping6 -c3 -I ${routeInterface} ${address}", returnStatus:true
+        rc = sh script: "ssh -i /var/node.pem ${sshUserName}@${ncmHost} ping6 -c3 -I ${routeInterface} $address", returnStatus:true
         //rc = sh script: "ssh -i ${env.WORKSPACE}/configuration/node.pem cloud-user@10.92.130.42 ping6 -c3 -I eth0 2a00:8a00:4000:020c:0000:0000:001c:001a",returnStatus:true
     }
     if (rc != 0) {
