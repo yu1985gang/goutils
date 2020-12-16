29adbdafcf87c2caed2abff33bfc0fa2136097be
ganyu <gerald.yu@nokia-sbell.com>
2020-12-16 23:22:48 +0800

    add files

--- scripts/Precheck.groovy
+++ scripts/Precheck.groovy
@@ -141,8 +141,7 @@ def pingNE(String address){
         assert address instanceof String
         echo "address is ${address}"
         echo "address is $address"
-        def ad = address.toString()
-        rc = sh script: "ssh -i ${env.WORKSPACE}/configuration/node.pem ${sshUserName}@${ncmHost} ping6 -c3 -I eth0 2a00:8a00:4000:020c:0000:0000:001c:001a", returnStatus:true
+        rc = sh script: "ssh -i ${env.WORKSPACE}/configuration/node.pem ${sshUserName}@${ncmHost} ping6 -c3 -I eth0 ${address}", returnStatus:true
         //rc = sh script: "ssh -i ${env.WORKSPACE}/configuration/node.pem cloud-user@10.92.130.42 ping6 -c3 -I eth0 2a00:8a00:4000:020c:0000:0000:001c:001a",returnStatus:true
     }
     if (rc != 0) {
