54e7fb37a0d9dea51f9b32b3a970a54b9478e97c
ganyu <gerald.yu@nokia-sbell.com>
2020-12-17 22:19:09 +0800

    add files

--- 1.groovy
+++ 1.groovy
@@ -1,17 +1,23 @@
-def filePath = "C:\\NOM\\neo0033\\node.pem"
-File fileReader = new File(filePath)
+def filePath = "C:\\git\\goutils\\configuration\\syve.yaml"
+//def filePath2 = "C:\\NOM\\neo0033\\node2.pem"
 
+print new File(filePath)
 
-def list = fileReader.readLines()
-print list.join('\n')
-
-
-
-
+def datas  = readYaml(file: filePath)
+print datas
 
 
 
-def filePath2 = "C:\\NOM\\neo0033\\node2.pem"
-File fileWriter = new File(filePath2)
+// def list = fileReader.readLines()
+// print list.join('\n')
+
+
+
+
+
+
+
+// def filePath2 = "C:\\NOM\\neo0033\\node2.pem"
+// File fileWriter = new File(filePath2)
 
 
