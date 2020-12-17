6b153b80e394bcc5c86e55404ca723cff326af50
ganyu <gerald.yu@nokia-sbell.com>
2020-12-17 12:24:35 +0800

    add files

--- scripts/Precheck.groovy
+++ scripts/Precheck.groovy
@@ -131,11 +131,11 @@ def pingAddressFromLocal(String address) {
     echo "1"
     if ( Utils.isIPv4(address) || Utils.isIPv4Fqdn(address)){
         echo "===> ipv4 address or ipv4 fqdn"
-        rc = sh script: "ping -c ${account} ${address}", returnStatus: true, label: "Ping ipv4 address"
+        rc = sh script: "ping -c3 ${address}", returnStatus: true, label: "Ping ipv4 address"
     }else if (Utils.isIPv6(address) || Utils.isIPv6Fqdn(address)){
         echo "===> ipv6 address or ipv6 fqdn"
         def intf = Utils.shCmd("netstat -rn | grep '^0.0.0.0' | rev | cut -d ' '  -f1 | rev")
-        rc = sh script: "ping6 -I ${intf}-c ${account} ${address}", returnStatus: true, label: "Ping ipv6 address"
+        rc = sh script: "ping6 -I ${intf}-c3 ${address}", returnStatus: true, label: "Ping ipv6 address"
     } else {
         error("Not a valid address: ${address}")
     }
