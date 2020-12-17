562b59fa08bec65de56efda605c412c270d8ce91
ganyu <gerald.yu@nokia-sbell.com>
2020-12-17 17:10:44 +0800

    add files

--- 1.groovy
+++ 1.groovy
@@ -1,2 +1,24 @@
 
 
+def filePath = "C:\\NOM\\neo0033\\node.pem"
+File file = new File(filePath)
+
+def filePath2 = "C:\\NOM\\neo0033\\node2.pem"
+File fileWriter = new File(filePath2)
+
+
+
+
+
+
+def list = file.readLines()
+
+
+file.eachLine {line ->
+	fileWriter.append(line)
+}
+
+
+print fileWriter.text
+
+
